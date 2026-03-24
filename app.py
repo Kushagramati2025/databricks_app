a21d7fe98a498fe67ffd76d0ff2c02b407cc61a9
abhishiek <abhishiek.work@gmail.com>
2026-03-24 17:12:59 +0530

    done

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import streamlit as st
 
-st.title("📊 Loan Analytics App")
+st.title("🚀 App is Working!")
 
-st.write("Welcome to your first Databricks App 🚀")
-
-# Sample data (temporary)
-data = {
-    "Loan ID": ["L001", "L002", "L003"],
-    "Total Due": [18000, 36000, 18000]
-}
-
-st.table(data)
+st.write("If you see this, deployment is successful ✅")
